383b7cd0f4faf816b4bfb1aac77116b76fb4e8ae
youssouf doucoure <madkdouc@yahoo.com>
2024-01-25 19:43:32 +0100

    fichier hF

---
+++ templates/footer.php
@@ -0,0 +1,51 @@
+
+<!--- début du foooter ---->
+
+    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+        <div class="col mb-3">
+            <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+                <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+            </a>
+            <p class="text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">© 2024</font></font></p>
+        </div>
+        <div class="col mb-3">
+
+        </div>
+
+<div class="col mb-3">
+        <h5><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Section</font></font></h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Maison</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Caractéristiques</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Tarifs</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">FAQ</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">À propos</font></font></a></li>
+            </ul>
+        </div>
+
+        <div class="col mb-3">
+        <h5><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Section</font></font></h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Maison</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Caractéristiques</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Tarifs</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">FAQ</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">À propos</font></font></a></li>
+            </ul>
+        </div>
+
+        <div class="col mb-3">
+        <h5><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Section</font></font></h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Maison</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Caractéristiques</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Tarifs</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">FAQ</font></font></a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">À propos</font></font></a></li>
+            </ul>
+        </div>
+    </footer>
+    </div>
+                
+</body>
+</html>
